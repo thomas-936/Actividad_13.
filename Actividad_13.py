29355be0a941074d3ecffd627b976fdef7eddd3c
tomas <tomas27@gmail.com>
2025-08-09 23:24:08 -0600

    5

--- Actividad_13.py
+++ Actividad_13.py
@@ -17,6 +17,20 @@ class EmpresaMensajeria:
     def __init__(self):
         self.repartidores = {}
 
+    def total_entregas(self, ):
+        suma = 0
+        for i in self.repartidores.values():
+            suma += i.cantidad_paquetes
+        return suma
+
+    def promedio_entregas(self):
+        total = self.total_entregas()
+        cantididad = len(self.repartidores)
+        promedio = total / cantididad
+        if cantididad == 0:
+            return  0
+        return promedio
+
 
     def busqueda_secuecial(self, objetivo):
         lista = list(self.repartidores.values())
@@ -33,7 +47,7 @@ class EmpresaMensajeria:
         busco = self.busqueda_secuecial(nombre)
         if busco != -1:
             print("El repartidor ha sido encontrado :) ")
-            print(nombre)
+            print(self.repartidores[nombre])
         else:
             print("El repartidor no ha sido encontrado")
 
@@ -55,18 +69,18 @@ class EmpresaMensajeria:
                     print("Este codigo ya ha sido resgistrado anteriormente....")
                 else:
                     break
-                nombre = input("Ingrese el nombre del rapartidor: ")
-                cantidad_paquetes = self.pedir_entero("Ingrese la cantidad de paquetes entregados: ")
-                zona = input("Ingrese la zona trabajada por el repartidor: ")
-                self.repartidores[nombre]= Repartidor(codigo, nombre, cantidad_paquetes, zona)
+            nombre = input("Ingrese el nombre del rapartidor: ")
+            cantidad_paquetes = self.pedir_entero("Ingrese la cantidad de paquetes entregados: ")
+            zona = input("Ingrese la zona trabajada por el repartidor: ")
+            self.repartidores[codigo] = Repartidor(codigo, nombre, cantidad_paquetes, zona)
 
     def quick_sort(self, lista):
-        if len(lista) == 1:
+        if len(lista) <= 1:
             return lista
         pivote = lista[0]
-        menores = [x for x in lista if x.cantidad_paquetes < pivote.catidad_paquetes]
-        iguales = [x for x in lista if x.cantidad_paquetes == pivote.catidad_paquetes ]
-        mayores = [x for x in  lista if x.cantidad_paquetes > pivote.catidad_paquetes]
+        menores = [x for x in lista if x.cantidad_paquetes < pivote.cantidad_paquetes]
+        iguales = [x for x in lista if x.cantidad_paquetes == pivote.cantidad_paquetes ]
+        mayores = [x for x in  lista if x.cantidad_paquetes > pivote.cantidad_paquetes]
         return self.quick_sort(mayores) + iguales + self.quick_sort(menores)
 
     def menu(self):
@@ -87,6 +101,23 @@ class EmpresaMensajeria:
                     lista_ordenada = self.quick_sort(list(self.repartidores.values()))
                     for repartidor in lista_ordenada:
                         print(repartidor)
-
-
+                case 3:
+                    self.buscar_repartidor()
+                case 4:
+                    print("El Ramking de entregas es: ")
+                    lista_ranking = self.quick_sort(list(self.repartidores.values()))
+                    for i in lista_ranking:
+                        print(i)
+                case 5:
+                    print("Menu de estadisticas")
+                    print("1. Total de paquestes entregados")
+                    print("2. Promedio de entregas")
+                    print("3. Repartidores con más entregas")
+                    print("4. Repartidores con menos emtregas")
+                    opcion_case5 = self.pedir_entero("Ingrese su opción: ")
+                    match opcion_case5:
+                        case 1:
+                            self.total_entregas()
+                        case 2:
+                            self.promedio_entregas()
 
